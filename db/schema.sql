3cf3aef90712d9a5484cd91b5ce89f65af4cc726
Bright Abety <bright@Brights-MacBook-Pro.local>
2023-01-08 16:53:54 -0500

    initial commit

---
+++ db/schema.sql
@@ -0,0 +1,37 @@
+DROP DATABASE IF EXISTS employeeDB;
+
+CREATE DATABASE employeeDB;
+
+USE employeeDB;
+
+DROP TABLE IF EXISTS department;
+
+CREATE TABLE department (
+  id INT unsigned NOT NULL AUTO_INCREMENT,
+  name varchar(30) NOT NULL,
+  PRIMARY KEY (id)
+);
+
+
+DROP TABLE IF EXISTS role;
+
+CREATE TABLE role (
+  id INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
+  title varchar(30) NOT NULL,
+  salary decimal(10,2) NOT NULL,
+  department_id int NOT NULL,
+  FOREIGN KEY (department_id) REFERENCES department(id)
+);
+
+
+DROP TABLE IF EXISTS employee;
+
+CREATE TABLE employee (
+  id INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
+  first_name varchar(30) DEFAULT NULL,
+  last_name varchar(30) DEFAULT NULL,
+  role_id int DEFAULT NULL,
+  manager_id int DEFAULT NULL,
+  FOREIGN KEY (role_id) REFERENCES role(id),
+  FOREIGN KEY (manager_id) REFERENCES employee(id)
+);
---
+++ db/seeds.sql
@@ -0,0 +1,21 @@
+INSERT INTO department (id, name)
+VALUES 
+        (),
+        (),
+        (),
+        (4);
+
+INSERT INTO role (id, title ,salary, department_id)
+VALUES 
+        (),
+        (),
+        (),
+        (),
+        ();
+
+INSERT INTO employee (id, first_name, last_name, role_id, manager_id)
+VALUES 
+        (),
+        (),
+        (),
+        ();
